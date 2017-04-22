6d84204ab5bc4096dfb222dc5545cc5f7e5d1ec9
Jen Yu <jyu7@stuy.edu>
2017-04-21 21:19:27 -0400

    added comments

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,24 +1,30 @@
-public class HelpDesk{
+//HelpDesk manages Tickets
+public class HelpDesk
+{
+	//Instance Variables
     private ArrayPriorityQueue<Ticket> problemQueue;
 
-    public HelpDesk(){
-	problemQueue = new ArrayPriorityQueue();
+	//Default Constructor
+    public HelpDesk()
+	{
+		problemQueue = new ArrayPriorityQueue();
     }
 
+	//toString() Method
     public String toString()
     {
-	return problemQueue.toString();
+		return problemQueue.toString();
     }
 
-
+	//how helpful is the help desk? 
     public static void main(String[] args)
     {
-	HelpDesk swann = new HelpDesk();
-	swann.problemQueue.add(new Ticket(5, "Bob", "LoL"));
-	swann.problemQueue.add(new Ticket(1, "Ha", "exploded"));
-	System.out.println(swann);
-	System.out.println(swann.problemQueue.removeMin());
-	System.out.println(swann.problemQueue.peekMin());
+		HelpDesk swann = new HelpDesk();
+		swann.problemQueue.add(new Ticket(5, "Bob", "LoL"));
+		swann.problemQueue.add(new Ticket(1, "Ha", "exploded"));
+		System.out.println(swann);
+		System.out.println(swann.problemQueue.removeMin());
+		System.out.println(swann.problemQueue.peekMin());
     }
 
 }
